41fae8c530f7642867cac8cfa9814bf7650f5b7d
jungwj0706 <woox1jin@gmail.com>
2025-09-24 03:51:25 +0900

    feat: add ModeSelectPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage.jsx';
 import NameInputPage from './pages/NameInputPage/NameInputPage.jsx';
-// import GameSelectPage from './pages/ModeSelectPage/ModeSelectPage.jsx';
+import GameSelectPage from './pages/ModeSelectPage/ModeSelectPage.jsx';
 // import WordGamePage from './pages/GamePage/WordGamePage.jsx';
 // import SentenceGamePage from './pages/GamePage/SentenceGamePage.jsx';
 // import RankingPage from './pages/RankingPage/RankingPage.jsx';
@@ -13,7 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/name" element={<NameInputPage />} />
-        {/* <Route path="/select" element={<GameSelectPage />} /> */}
+        <Route path="/select" element={<GameSelectPage />} />
         {/* <Route path="/game/word" element={<WordGamePage />} /> */}
         {/* <Route path="/game/sentence" element={<SentenceGamePage />} /> */}
         {/* <Route path="/ranking" element={<RankingPage />} /> */}

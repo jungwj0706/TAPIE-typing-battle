ccd8557433a1561fac3b6c0334d1f63b124b3ac0
jungwj0706 <woox1jin@gmail.com>
2025-09-25 01:17:07 +0900

    feat: add GamePage(Word&Sentence)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import MainPage from './pages/MainPage/MainPage.jsx';
-import NameInputPage from './pages/NameInputPage/NameInputPage.jsx';
-import GameSelectPage from './pages/ModeSelectPage/ModeSelectPage.jsx';
-// import WordGamePage from './pages/GamePage/WordGamePage.jsx';
-// import SentenceGamePage from './pages/GamePage/SentenceGamePage.jsx';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage.jsx";
+import NameInputPage from "./pages/NameInputPage/NameInputPage.jsx";
+import GameSelectPage from "./pages/ModeSelectPage/ModeSelectPage.jsx";
+import WordGamePage from "./pages/GamePage/WordGamePage.jsx";
+import SentenceGamePage from "./pages/GamePage/SentenceGamePage.jsx";
 // import RankingPage from './pages/RankingPage/RankingPage.jsx';
 
 export default function App() {
@@ -14,8 +14,8 @@ export default function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/name" element={<NameInputPage />} />
         <Route path="/select" element={<GameSelectPage />} />
-        {/* <Route path="/game/word" element={<WordGamePage />} /> */}
-        {/* <Route path="/game/sentence" element={<SentenceGamePage />} /> */}
+        <Route path="/game/word" element={<WordGamePage />} />
+        <Route path="/game/sentence" element={<SentenceGamePage />} />
         {/* <Route path="/ranking" element={<RankingPage />} /> */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
